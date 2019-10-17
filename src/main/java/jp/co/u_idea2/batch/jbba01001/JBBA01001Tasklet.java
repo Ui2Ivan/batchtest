65787b8024c1d3aca007648b118f569a0da5b791
Ui2Ivan <45223464+Ui2Ivan@users.noreply.github.com>
2019-10-17 12:27:42 +0900

    Change Structure

--- src/main/java/jp/co/u_idea2/batch/jbba01001/JBBA01001Tasklet.java
+++ src/main/java/jp/co/u_idea2/batch/jbba01001/JBBA01001Tasklet.java
@@ -209,10 +209,11 @@ public class JBBA01001Tasklet implements Tasklet {
         mail.setSubject(" ");
         mail.setFrom(from);
         mail.setReplyTo(from);
-        mail.setTemplateId("d-1dea9b705b854c9786cfafbbbdba835c");
+        mail.setTemplateId("d-41698dc549a54b7b9f9867ece5c85685");
         Personalization personalization = new Personalization();
         final JsonObject json = new com.google.gson.JsonObject();
-        json.addProperty("-customername-", "contact@aimmoth.com");
+        json.addProperty("-name-", "contact@aimmoth.com");
+        json.addProperty("name", "User1");
         personalization.addCustomArg("dynamic_template_data", json.toString());
         personalization.addDynamicTemplateData("customername", "User1");
         personalization.addDynamicTemplateData("-customername-", "User2");
